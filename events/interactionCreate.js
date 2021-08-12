222cc369f89bbb2ebe6b0d7f46f2b1240b73514e
ChickenMax <li-hk@hotmail.com>
2021-08-13 00:44:08 +0800

    minor change

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,14 +1,14 @@
 module.exports = {
     name: "interactionCreate",
     on: true,
-    async run(interaction, commands){
+    run(interaction, commands){
         //Disable access for specific user. For bullying purpose :D
         if(interaction.user.id == "333985543324958734"){
             interaction.reply("Fuck off");
             return;
         }
 
-        if(interaction.isCommand()) //commands list might get large and takes some time to search so use async here just in case
-            await commands.get(interaction.commandName).execute(interaction);
+        if(interaction.isCommand())
+            commands.get(interaction.commandName).execute(interaction);
     }
 }
