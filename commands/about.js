967f92d5b96677c3ffaefefbcfadc921a3bd8df8
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2022-02-21 14:45:10 +0800

    updated the github link

--- commands/about.js
+++ commands/about.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription("Information of the bot."),
 	execute(interaction) {
 		interaction.reply(
-            "This is a discord bot created by Ch1ckenMax\n Discord ID: ChickenMax#8201\n GitHub Repo Link: https://github.com/Ch1ckenMax/discord_bot_for_fun"
+            "This is a discord bot created by Ch1ckenMax\n Discord ID: ChickenMax#8201\n GitHub Repo Link: https://github.com/Ch1ckenMax/discord_f1_bot"
         );
 	},
 };
